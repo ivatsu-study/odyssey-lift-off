71de4a55c215238d9683283ac5b6943d8a25ffa0
IvaTsu <ivan.tsud@1337.tech>
2022-09-06 16:34:23 +0200

    feat: implement useQuery to fetch data

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
 import { Layout } from '../components';
+import QueryResult from '../components/query-result';
+import TrackCard from '../containers/track-card';
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  return (
+    <QueryResult loading={loading} error={error} data={data}>
+      <Layout grid>{data?.tracksForHome?.map(track => <TrackCard key={track.id} track={track} />)}</Layout>
+    </QueryResult>
+  );
 };
 
 export default Tracks;
